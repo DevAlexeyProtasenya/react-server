952ff189e6844bdd5329fb891fd3594881797742
DevAlexeyProtasenya <283fg56j98@gmail.com>
2021-09-14 19:07:09 +0300

    feat: add deleteRoom

---
+++ src/endpoints/deleteRooms.ts
@@ -0,0 +1,25 @@
+import { Server, Socket } from "socket.io";
+import { deleteRoom } from "../rooms";
+import { deleteUser, getUsers } from "../users";
+
+const deleteUserFromRoom = (socket: Socket, io: Server) => {
+  socket.on("deleteRoom", ({roomID}, callback) => {
+    io.in(roomID).emit('cancelGame');
+    const users = getUsers(roomID);
+    users.forEach(user => {
+      deleteUser(user.getId());
+    });
+    const room = deleteRoom(roomID);
+    if (room) {
+      return callback(JSON.stringify({
+        status: 200,
+      }));
+    }
+    return callback(JSON.stringify({
+      status: 404,
+      message: "Room not found!",
+    }));
+  });
+}
+
+export default deleteUserFromRoom;
--- src/endpoints/login.ts
+++ src/endpoints/login.ts
@@ -15,7 +15,6 @@ const login = (socket: Socket, io: Server) => {
       }));
     }
     socket.join(user.getRoom());
-    socket.in(user.getRoom()).emit('notification', { title: 'Someone\'s here', description: `${user.getName()} just entered the room` });
     io.in(user.getRoom()).emit('users', getUsers(user.getRoom()));
     console.log(user);
     callback(JSON.stringify({
